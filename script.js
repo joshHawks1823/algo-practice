e211a4b79dc85a7a2667496914d827be4b071b96
joshhawks1823 <jheat23@gmail.com>
2022-12-05 22:11:12 -0500

    two sum

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hi')
+// Two Number Sum 
+
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+ You may assume that each input would have exactly one solution, and you may not use the same element twice. */
+
+
+
+function twoSum(nums, target) {
+
+	const comp = new Map();
+	const len = nums.length;
+
+	for (let i = 0; i < len; i++) {
+		if(comp[nums[i]] >= 0) {
+			return [comp[nums[i]], i]
+		}
+		comp[target - nums[i]] = i;
+	}
+	return [];
+	}
+
+const nums = [2, 7, 11, 15];
+
+console.log(twoSum(nums, 9))
